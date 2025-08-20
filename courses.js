5e5cdae36cd50fda26601fe4b1d784f244915f38
chrismedz <chrismedz@hotmail.co.uk>
2025-08-20 20:53:38 +0100

    Update and rename data to courses.js

---
+++ courses.js
@@ -0,0 +1,54 @@
+// Sample dataset (expand with real info)
+const courses = [
+  {
+    id: 1,
+    name: "St Andrews (Old Course)",
+    county: "Fife",
+    type: "Links",
+    length: 6721,
+    par: 72,
+    slope: 132,
+    difficulty: 5,
+    fees: "£270 weekday visitor",
+    address: "West Sands, St Andrews, KY16 9XL",
+    contact: "01334 466666",
+    lat: 56.3429,
+    lng: -2.8030,
+    image: "images/standrews.jpg",
+    youtube: "ZyDQpE7YgX0"
+  },
+  {
+    id: 2,
+    name: "Royal Birkdale Golf Club",
+    county: "Merseyside",
+    type: "Links",
+    length: 7133,
+    par: 72,
+    slope: 139,
+    difficulty: 5,
+    fees: "£225 visitor",
+    address: "Waterloo Rd, Southport PR8 2LX",
+    contact: "01704 552020",
+    lat: 53.6262,
+    lng: -3.0288,
+    image: "images/royalbirkdale.jpg",
+    youtube: "n0-7t87jqRw"
+  },
+  {
+    id: 3,
+    name: "Sunningdale (Old)",
+    county: "Berkshire",
+    type: "Heathland",
+    length: 6617,
+    par: 70,
+    slope: 136,
+    difficulty: 4,
+    fees: "£275 weekday visitor",
+    address: "Ridgemount Rd, Ascot SL5 9RR",
+    contact: "01344 621681",
+    lat: 51.385,
+    lng: -0.633,
+    image: "images/sunningdale.jpg",
+    youtube: "xKv8d7b2dWY"
+  }
+];
